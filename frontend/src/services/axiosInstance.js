226718fc9656f5e36d0f75e80e75b9b46cdec166
pankaj2312002 <pankaj707165@gmail.com>
2025-02-05 18:26:44 +0530

    Backend URL change

--- frontend/src/services/axiosInstance.js
+++ frontend/src/services/axiosInstance.js
@@ -4,8 +4,7 @@ import axios from 'axios';
 const baseURL = process.env.REACT_APP_BASE_URL;
 // 'http://localhost:8080/api/v1'
 const axiosInstance = axios.create({
-  // baseURL: "https://spendr-expense-tracker.vercel.app/api/v1",
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: "https://spendr-expense-tracker.vercel.app/api/v1",
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
